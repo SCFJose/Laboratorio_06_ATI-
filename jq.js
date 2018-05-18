bffc0518e9ae417f1075bc1a6ced45e9a3783907
Jose <scfjose.h.h@gmail.com>
2018-05-18 17:57:32 -0400

    modificacion del js

--- jq.js
+++ jq.js
@@ -5,19 +5,19 @@ $(document).ready(function() {
 $(document).ajaxError(function(e, jqXHR, settings, exception) {
 	switch (jqXHR.status) {
 		case 0:
-			alert("404 Page Not Found");
+			alert("Pagina no encontrada");
 	        break;
 	    case 200:
-	        alert("200 OK!");
+	        alert("OK!");
 	        break;
 	    case 403:
 	    	alert("403 Forbidden");
 	    	break;
 	    case 404:
-	        alert("404 Page Not Found");
+	        alert("404 Pagina no encontrada");
 	        break;
 	    case 500:
-	    	alert("500 Internal Server Error");
+	    	alert("500 Error interno del servidor");
 	        break;
 	    default:
 	            alert("Un error ha ocurrido: " + jqXHR.status);
